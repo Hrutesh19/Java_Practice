e382e21e3542484f16367a87c97977bd8fd4dbc4
Hrutesh Ghodmare <hruteshghodmare19@gmail.com>
2023-09-06 11:16:35 +0530

    commit -m " Added linearSearch code"

--- LinearSearch.java
+++ LinearSearch.java
@@ -2,30 +2,41 @@ import java.util.Scanner;
 
 public class LinearSearch {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the length of the array: ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Enter the values of the array: ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        System.out.print("Enter the value to search for: ");
-        int x = sc.nextInt();
-        int index = linearSearch(arr, x);
-        if (index == -1) {
-            System.out.println(x + " is not present in the array.");
-        } else {
-            System.out.println(x + " is present at index " + index + ".");
-        }
-    }
+        Scanner scanner = new Scanner(System.in);
 
-    public static int linearSearch(int[] arr, int x) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == x) {
-                return i;
+        // Input the length of the array
+        System.out.print("Enter the length of the array: ");
+        int length = scanner.nextInt();
+
+        // Create an array of the specified length
+        int[] arr = new int[length];
+
+        // Input array elements from the user
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        // Input the value to search for
+        System.out.print("Enter the value to search for: ");
+        int searchValue = scanner.nextInt();
+
+        // Perform linear search
+        boolean found = false;
+        for (int i = 0; i < length; i++) {
+            if (arr[i] == searchValue) {
+                found = true;
+                break; // Found the value, no need to continue searching
             }
         }
-        return -1;
+
+        // Check if the value was found or not
+        if (found) {
+            System.out.println("The value was found in the array.");
+        } else {
+            System.out.println("The value was not found in the array.");
+        }
+
+        scanner.close();
     }
 }
